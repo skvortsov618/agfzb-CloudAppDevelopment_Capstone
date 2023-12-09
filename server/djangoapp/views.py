7cd9c6c55f1e5364998cb9b78763a7df9d5dfb1c
Dmitrii Skvortsov <618skvortsov@gmail.com>
2023-12-08 19:41:11 -0500

    first part ready

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,14 +16,27 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-
 # Create an `about` view to render a static about page
 # def about(request):
-# ...
+def get_about(request):
+    context = {}
+    # If the request method is GET
+    if request.method == 'GET':
+        # Using the objects model manage to read all course list
+        # and sort them by total_enrollment descending
+        return render(request, 'djangoapp/about.html', context)
+
 
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
+def get_contact(request):
+    context = {}
+    # If the request method is GET
+    if request.method == 'GET':
+        # Using the objects model manage to read all course list
+        # and sort them by total_enrollment descending
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
